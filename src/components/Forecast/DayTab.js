06550733addcbbb81e9576a2b39bb70836fb152d
Mihajlo Jovanovic <michael@accusys.co>
2018-10-25 14:44:07 +0200

    forecast tabs blur

--- src/components/Forecast/DayTab.js
+++ src/components/Forecast/DayTab.js
@@ -3,9 +3,12 @@ import React from 'react'
 export function DayTab(){
   return(
     <div className='tab'>
-      <img className='icon' alt='icon'/>
-      <span>32</span>
-      <span>SUN</span>
+      <div className='tab--blur'></div>
+      <div className='tab--content'>
+        <img className='icon' alt='icon'/>
+        <span>32</span>
+        <span>SUN</span>
+      </div>
     </div>
   )
 }
--- src/components/Today/General.js
+++ src/components/Today/General.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import FakeChart from '../UI/fakeChart'
+import sun from '../../assets/icons/sun.svg'
 
 export function TodayGeneral(){
   return(
     <div className='widget widget__current widget__current--general'>
+      <div className='general-row'>
+        <div className='temp'>
+          <span className='degrees'>30°</span>
+          <span className='sky'>Sunny</span>
+        </div>
+        <img className='sun-icon' src={sun} alt='sun icon' height={100}/>
+      </div>
       <FakeChart/>
     </div>
   )
